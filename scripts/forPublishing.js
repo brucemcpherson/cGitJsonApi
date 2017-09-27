b779bd19d0e736cd0f5562c0838dfeea73dfd167
Bruce McPherson <bruce@mcpher.com>
2017-09-27 14:02:52 +0100

    updated by GasGit automation

--- scripts/forPublishing.js
+++ scripts/forPublishing.js
@@ -7,7 +7,7 @@ function getLibraryInfo () {
   return { 
     info: {
       name:'cGitJsonApi',
-      version:'0.0.2',
+      version:'0.0.3',
       key:'M5Z3qjwycVC9Cebh0UEFuIKi_d-phDA33',
       description:'library to interact with git api',
       share:'https://script.google.com/d/1_4RfsIW57fdzWh7T38O9IfGdTRgYbOSyC5PvsOm3a4GU1sxllw8blEUl/edit?usp=sharing'
